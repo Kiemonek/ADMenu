387ce2692095ba2e52c70bc6810eb658deae2187
Kiemonek <ytoconner@gmail.com>
2023-10-07 23:10:44 +0200

    Close add button window

--- AddNewButton.py
+++ AddNewButton.py
@@ -1,36 +1,38 @@
-import json
+# import json
 from tkinter import *
-from tkinter.ttk import *
-
-class AddNewButton():
     
+class AddNewButton():
+
     def __init__(self, root):
         self.root = root
         self.root.title("Add new button")
-        self.root.geometry("200x500")
+        # self.root.geometry("200x500")
         
-        self.label = Label(root, text="Insert data")
-        self.label.pack()
+        self.label = Label(root, text="Insert data").grid()
         
-        self.label1 = Label(root, text="Domain:")
-        self.label1.pack()
+        self.label_Domain = Label(root, text="Domain:").grid()
         
-        self.textDomain = Text(root, height=2)
-        self.textDomain.pack()
+        self.textDomain = Text(root, height=2).grid()
         
-        self.label2 = Label(root, text="Username:")
-        self.label2.pack()
+        self.label_Username = Label(root, text="Username:").grid()
         
-        self.textUsername = Text(root, height=2)
-        self.textUsername.pack()
+        self.textUsername = Text(root, height=2).grid()
         
-        self.label3 = Label(root, text="Server:")
-        self.label3.pack()
+        self.label_Server = Label(root, text="Server:").grid()
         
-        self.textServer = Text(root, height=2)
-        self.textServer.pack()
+        self.firstOctet = Text(root, height=2, width=4).grid()
         
-        self.button = Button(root, text="SAVE")
-        self.button.pack()
-        self.button = Button(root, text="EXIT")
-        self.button.pack()
+        self.label_dot1 = Label(root, text=".").grid()
+        
+        self.secOctet = Text(root, height=2, width=4).grid()
+        
+        self.label_dot2 = Label(root, text=".").grid()
+        
+        self.thirdOctet = Text(root, height=2, width=4).grid()
+        
+        self.label_dot3 = Label(root, text=".").grid()
+        
+        self.fourthOctet = Text(root, height=2, width=4).grid()
+        
+        self.button = Button(root, text="SAVE").grid()
+        self.button = Button(root, text="EXIT", command=lambda :root.destroy()).grid()
--- Main.py
+++ Main.py
@@ -3,17 +3,15 @@ from tkinter.ttk import *
 from DashboardButton import *
 from AddNewButton import AddNewButton
 import os
-# Implement list from json
-buttonList = DashboardButton.getButtonList()
 
 def add_new_button():
     newButton = Toplevel(root)
     app = AddNewButton(newButton)
 
-# Initialize variable for columns and rows
-colHelper = 0
-rowHelper = 0
+# Implement list from json
+buttonList = DashboardButton.getButtonList()
 
+#Customize Window
 root = Tk()
 root.title("AD Menu")
 root.geometry("640x480")
@@ -22,9 +20,8 @@ root.geometry("640x480")
 for items in buttonList:
     button = Button(root, text = items.title, command = items.onPressed)
     button.grid()
-    # print(buttonList.index(items))
-    # print(0%2)
 
+#Usable Buttons
 butt = Button(root, text = "Add New Button", command = add_new_button)
 butt.grid()
 butt = Button(root, text = "Modify Buttons" )
