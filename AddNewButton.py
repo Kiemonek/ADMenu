1c1a65a0e44c27b1ca07b8bdc951ed057d9e0d0e
Kiemonek <ytoconner@gmail.com>
2023-10-14 12:51:13 +0200

    modify Text widgets to Entry and add getters

--- AddNewButton.py
+++ AddNewButton.py
@@ -2,37 +2,75 @@
 from tkinter import *
     
 class AddNewButton():
-
+    global appendButton 
+    # appendButton = []
+    
     def __init__(self, root):
         self.root = root
         self.root.title("Add new button")
-        # self.root.geometry("200x500")
+        self.root.geometry("200x500")
         
-        self.label = Label(root, text="Insert data").grid()
+        self.label = Label(root, text="Insert data")
+        self.label.grid()
         
-        self.label_Domain = Label(root, text="Domain:").grid()
+        self.label_Name = Label(root, text="Name:")
+        self.label_Name.grid()
         
-        self.textDomain = Text(root, height=2).grid()
+        self.e_Name = Entry(root)
+        self.e_Name.grid()
         
-        self.label_Username = Label(root, text="Username:").grid()
+        self.label_Domain = Label(root, text="Domain:")
+        self.label_Domain.grid()
         
-        self.textUsername = Text(root, height=2).grid()
+        self.e_Domain = Entry(root)
+        self.e_Domain.grid()
         
-        self.label_Server = Label(root, text="Server:").grid()
+        self.label_Username = Label(root, text="Username:")
+        self.label_Username.grid()
         
-        self.firstOctet = Text(root, height=2, width=4).grid()
+        self.e_Username = Entry(root)
+        self.e_Username.grid()
         
-        self.label_dot1 = Label(root, text=".").grid()
+        self.label_Server = Label(root, text="Server:")
+        self.label_Server.grid()
         
-        self.secOctet = Text(root, height=2, width=4).grid()
+        self.e_fOctet = Entry(root)
+        self.e_fOctet.grid()
         
-        self.label_dot2 = Label(root, text=".").grid()
+        self.label_dot1 = Label(root, text=".")
+        self.label_dot1.grid()
         
-        self.thirdOctet = Text(root, height=2, width=4).grid()
+        self.e_sOctet = Entry(root)
+        self.e_sOctet.grid()
         
-        self.label_dot3 = Label(root, text=".").grid()
+        self.label_dot2 = Label(root, text=".")
+        self.label_dot2.grid()
         
-        self.fourthOctet = Text(root, height=2, width=4).grid()
+        self.e_tOctet = Entry(root)
+        self.e_tOctet.grid()
         
-        self.button = Button(root, text="SAVE").grid()
-        self.button = Button(root, text="EXIT", command=lambda :root.destroy()).grid()
+        self.label_dot3 = Label(root, text=".")
+        self.label_dot3.grid()
+        
+        self.e_foOctet = Entry(root)
+        self.e_foOctet.grid()
+        
+        self.saveButton = Button(root, text="SAVE", command=self.save)
+        self.saveButton.grid()
+        
+        self.exitButton = Button(root, text="EXIT", command=lambda :root.destroy())
+        self.exitButton.grid()
+        
+    def save(self):
+        # return
+        # server = self.e_fOctet.get() + "." + self.e_sOctet.get() + "." + self.e_tOctet.get() + "." + self.e_foOctet.get()
+        # print(server)
+        appendButton = [
+            None, 
+            self.e_Name.get(), 
+            self.e_Domain.get(), 
+            self.e_Username.get(),
+            self.e_fOctet.get() + "." + self.e_sOctet.get() + "." + self.e_tOctet.get() + "." + self.e_foOctet.get()
+            ]
+        print(appendButton)
+        
