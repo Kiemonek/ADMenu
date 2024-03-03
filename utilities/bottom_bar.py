cab9ef07b6b9c9ffd2c9bffdfc9df3d9e6b44873
Kiemonek <ytoconner@gmail.com>
2024-03-04 00:22:00 +0100

    Refactor bottom bar creation in Utilities class

---
+++ utilities/bottom_bar.py
@@ -0,0 +1,68 @@
+"""This module creates the bottom bar of the application with buttons to interact."""
+import tkinter as tk
+
+from app.utilities import util
+
+
+class BottomBar:
+    """This class creates the bottom bar of the application with buttons to interact."""
+
+    def __init__(self, frame, bottom_bar):
+
+        util.show_button_list(frame, "cmd")
+
+        dsa_button = tk.Button(
+            bottom_bar,
+            text="CONNECT",
+            bg='#1E1E1E',
+            fg='#838383',
+            font=("Microsoft YaHei", 12, "bold"),
+            activebackground='#838383',
+            command=lambda: [util.show_button_list(frame, "cmd")])
+        dsa_button.place(relwidth=0.23,
+                         relheight=0.7,
+                         anchor='n',
+                         relx=0.125,
+                         rely=0.15)
+
+        add_button = tk.Button(bottom_bar,
+                               text="ADD",
+                               bg='#1E1E1E',
+                               fg='#838383',
+                               font=("Microsoft YaHei", 12, "bold"),
+                               activebackground='#838383',
+                               command=lambda: [util.buttonDetails(frame)])
+        add_button.place(relwidth=0.23,
+                         relheight=0.7,
+                         anchor='n',
+                         relx=0.375,
+                         rely=0.15)
+
+        mod_button = tk.Button(
+            bottom_bar,
+            text="MODIFY",
+            bg='#1E1E1E',
+            fg='#838383',
+            font=("Microsoft YaHei", 12, "bold"),
+            activebackground='#838383',
+            command=lambda: [util.show_button_list(frame, "mod")])
+
+        mod_button.place(relwidth=0.23,
+                         relheight=0.7,
+                         anchor='n',
+                         relx=0.625,
+                         rely=0.15)
+
+        rem_button = tk.Button(
+            bottom_bar,
+            text="REMOVE",
+            bg='#1E1E1E',
+            fg='#838383',
+            font=("Microsoft YaHei", 12, "bold"),
+            activebackground='#838383',
+            command=lambda: [util.show_button_list(frame, "rm")])
+        rem_button.place(relwidth=0.23,
+                         relheight=0.7,
+                         anchor='n',
+                         relx=0.875,
+                         rely=0.15)
